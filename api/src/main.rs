18f2119a5c23297cfd828fe34a4c985cfe8a4b60
Aniket Singh <aniketsingh2151@gmail.com>
2025-07-22 23:54:46 +0530

    configured routes, need to figure out how to create 2 handlers for same endpoint in poem

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,7 +1,22 @@
-use std::fmt::Error;
+use poem::{Route, Server, get, post, handler, listener::TcpListener, web::Path};
+
+
+#[handler]
+fn get_web_status(Path(status): Path<String>) -> String {
+    format!("status: {}", status)
+}
+
+#[handler]
+fn post_web_status(Path(status): Path<String>) -> String {
+    format!("status: {}", status)
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    print!("Hellow");
-    Ok(())
+async fn main() -> Result<(), std::io::Error> {
+    let app: Route = Route::new()
+        .at("/status/:status", get(get_web_status))
+        .at("/status/:status",post(post_web_status));
+    Server::new(TcpListener::bind("0.0.0.0:3000"))
+        .run(app)
+        .await
 }
