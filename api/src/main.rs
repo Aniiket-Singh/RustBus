5a4021709aed59df5d8d7caf6966ce2ab2ead24f
Aniket Singh <aniketsingh2151@gmail.com>
2025-07-23 23:51:40 +0530

    input/output serialization/desiarlization between string<->json type done using serde

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,21 +1,26 @@
-use poem::{Route, Server, get, post, handler, listener::TcpListener, web::Path};
+use poem::{get, handler, listener::TcpListener, post, web::{Json, Path}, Route, Server};
 
+use crate::request_inputs::CreateWebsite;
+
+pub mod request_inputs;
+pub mod request_outputs;
 
 #[handler]
-fn get_web_status(Path(status): Path<String>) -> String {
+fn get_website_status(Path(status): Path<String>) -> String {
     format!("GET status: {}", status)
 }
 
 #[handler]
-fn post_web_status(Path(status): Path<String>) -> String {
-    format!("POST status: {}", status)
+fn create_website(Json(data): Json<CreateWebsite>) -> String {
+    format!("POST status")
 }
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     
     let app = Route::new()
-        .at("/status/:status", get(get_web_status).post(post_web_status));
+        .at("/status/:status", get(get_website_status))
+        .at("/website", post(create_website));
         
     
     
---
+++ api/src/request_inputs.rs
@@ -0,0 +1,7 @@
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Serialize, Deserialize)]
+pub struct CreateWebsite {
+    pub url: String
+}
